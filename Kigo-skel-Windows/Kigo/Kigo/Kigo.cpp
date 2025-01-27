39d3809ae3990c82bf8bb8d4aec591afb6a58d0c
Ben Turcotte <ben.turcotte@gmail.com>
2025-01-27 14:34:52 -0500

    Add files via upload

---
+++ Kigo-skel-Windows/Kigo/Kigo/Kigo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/*
+* Program Name: Kigo
+* Made by Ben Turcotte, 1/21/2025
+* Input:None
+* Outputs:string
+* Description: Program outputs a haiku.
+*/
+
+int main() {
+	std::cout << "Sunny Everyday." << std::endl;
+	std::cout << "Waiting for the chilling rain\n"
+		      << "to yield to my fears." << std::endl;
+	std::cout << "A haiku by Ben Turcotte, 2025.";
+
+	return 0;
+}
